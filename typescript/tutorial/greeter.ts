e1e778b96ead2713624682c9f1b1576854df29e4
Hervito <herve.ahleung@gmail.com>
2019-10-06 16:08:18 +0100

    HAL > TS > Complete TypeScript in 5 minutes

--- typescript/tutorial/greeter.ts
+++ typescript/tutorial/greeter.ts
@@ -1,10 +1,30 @@
-function greeter(person) {
-  return "Hello, " + person;
+// From: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+// Public key words on the constructor: macro to create queryable atts
+// Student comply to Person as the fields from the interface are implemented
+
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = "Jane User";
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
 
-console.log(greeter(user))
+// This struct is an implementation of Person interface
+let userInterface = { firstName: "Jane", lastName: "User" };
+// Invokes the only constructor of the Student class
+let student = new Student("Jane", "M.", "User");
 
+console.log(greeter(userInterface))
+console.log(greeter(student))
+console.log(student.fullName)
 
 // document.body.textContent = greeter(user);
