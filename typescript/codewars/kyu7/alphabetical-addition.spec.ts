58cc5eb5477e128f31de1cdb729c3fb048151caf
Hervito <herve.ahleung@gmail.com>
2019-10-14 21:46:26 +0100

    HAL > TS > Kyu-7 > Alphabetical addition

---
+++ typescript/codewars/kyu7/alphabetical-addition.spec.ts
@@ -0,0 +1,24 @@
+import {addLetters} from "./alphabetical-addition";
+
+interface Case {
+    inputs: string[]
+    expectation: string
+}
+
+describe("Fixed tests", () => {
+    const tests: Case[] = [
+        {inputs: ['a', 'b', 'c'], expectation: 'f'},
+        {inputs: ['z'], expectation: 'z'},
+        {inputs: ['a', 'b'], expectation: 'c'},
+        {inputs: ['c'], expectation: 'c'},
+        {inputs: ['z', 'a'], expectation: 'a'},
+        {inputs: ['y', 'c', 'b'], expectation: 'd'},
+        {inputs: [], expectation: 'z'}
+    ];
+
+    it("Tests", () => {
+        for (let testCase of tests) {
+            expect(addLetters(testCase.inputs)).toEqual(testCase.expectation);
+        }
+    })
+});
---
+++ typescript/codewars/kyu7/alphabetical-addition.ts
@@ -0,0 +1,13 @@
+// export function addLetters(...letters: string[]) {
+// }
+
+function f(ls: string[]) {
+    if (ls.length == 0) return 'z';
+
+    return ls.reduce((a, i) => (
+        String.fromCharCode(96 + ((a.charCodeAt(0) - 96 + i.charCodeAt(0) - 96) % 26))
+    ))
+}
+
+// console.log(f(["z", "y", "q", "y", "k"]));
+// console.log(f(["a", "b", "c"]));
