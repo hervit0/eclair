cb78378a7d6b3b69dee2f37a7fd5c0bfc82e08bc
Hervinator <hervito.dela.vega@gmail.com>
2022-12-05 13:39:34 +0000

    HAL > Go > AdventCalendar2022 > day 1 > part 2

--- go/adventCalendar2022/1/main.go
+++ go/adventCalendar2022/1/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	log "github.com/sirupsen/logrus"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	f, _ := os.ReadFile("adventCalendar2022/1/input.txt")
-	//log.WithField("resp", string(f)).Info("input")
-
 	ss := string(f)
+	part1(ss)
+	part2(ss)
+}
+
+func part1(ss string) {
 	interElvesSep := "\r\n\r\n"
 	interElfSep := "\r\n"
 	interElves := strings.Split(ss, interElvesSep)
@@ -32,4 +36,31 @@ func main() {
 	}
 
 	log.WithField("maxCalory", maxCalory).Info("part 1 answer")
+}
+
+func part2(ss string) {
+	interElvesSep := "\r\n\r\n"
+	interElfSep := "\r\n"
+	interElves := strings.Split(ss, interElvesSep)
+
+	elvesCalories := make([]int, len(interElves))
+	for idx, elf := range interElves {
+		elfCalories := strings.Split(elf, interElfSep)
+		count := 0
+		for _, caloryStr := range elfCalories {
+			calory, _ := strconv.Atoi(caloryStr)
+			count = count + calory
+		}
+
+		elvesCalories[idx] = count
+	}
+
+	sort.SliceStable(elvesCalories, func(i, j int) bool { return elvesCalories[i] > elvesCalories[j] })
+	calories := 0
+	for _, calory := range elvesCalories[0:3] {
+		log.WithField("calory", calory).Info("calory")
+		calories = calories + calory
+	}
+
+	log.WithField("calories", calories).Info("part 2 answer")
 }
